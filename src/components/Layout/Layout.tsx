262b905aff82250e43225369a38354e82cca7255
Guilherme Faxina <gamerstarbr7@hotmail.com>
2023-12-13 07:50:00 -0300

    feat: add header to layout

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,10 +1,12 @@
 import Aside from "./Aside";
+import Header from "./Header";
 
 
 export default function Layout() {
   return (
-    <>
+    <main className="flex flex-row h-screen">
       <Aside />
-    </>
+      <Header />
+    </main>
   )
 }
