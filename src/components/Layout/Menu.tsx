4159a3952fe9f229c47b67dd9a0b2def574f7bc2
Guilherme Faxina <gamerstarbr7@hotmail.com>
2023-12-14 14:17:18 -0300

    chore: change aside to fixed

--- src/components/Layout/Menu.tsx
+++ src/components/Layout/Menu.tsx
@@ -3,7 +3,7 @@ import { Avatar, AvatarImage } from "../ui/avatar";
 
 export default function Menu() {
   return (
-    <aside className="w-16 h-screen border-r-[1px] flex flex-col items-center">
+    <aside className="w-16 h-screen border-r-[1px] flex flex-col items-center fixed bg-white">
       <Avatar className="mt-8 mb-8 border-[1px] border-cyan-400 border-opacity-10">
         <AvatarImage src="https://images.unsplash.com/photo-1613324996029-f6190a17838f?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
       </Avatar>
