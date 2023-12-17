72f6f21e2e606335dce137621736d9bd16f44911
Guilherme Faxina <gamerstarbr7@hotmail.com>
2023-12-17 14:03:41 -0300

    chore: turn possible to style component

--- src/components/Layout/PersonalInfo.tsx
+++ src/components/Layout/PersonalInfo.tsx
@@ -1,15 +1,16 @@
 type TProps = {
   subtitle: string
+  className?: string
 }
 
-export default function PersonalInfo(props: TProps) {
+export default function PersonalInfo({subtitle, className}: TProps) {
   return (
-    <div>
+    <div className={className}>
       <h1 className="text-lg font-medium text-[#505050] mb-2">
         Informações pessoais
       </h1>
       <h2 className="text-sm text-[#909090]">
-        {props.subtitle}
+        {subtitle}
       </h2>
     </div>
   );
