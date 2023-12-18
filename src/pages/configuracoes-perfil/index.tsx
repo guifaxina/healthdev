f5854e0658675192c33f067058ceb7bc4af6150c
Guilherme Faxina <gamerstarbr7@hotmail.com>
2023-12-17 22:30:28 -0300

    feat: create placeholder for initial page

---
+++ src/pages/configuracoes-perfil/index.tsx
@@ -0,0 +1,9 @@
+import Layout from "@/components/Layout/Layout";
+
+export default function ConfigPerfil() {
+  return (
+    <div>
+      <Layout />
+    </div>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
-import Layout from "@/components/Layout/Layout"
+import Link from "next/link"
 
 export default function Home() {
   return (
-    <main>
-      <Layout />
-    </main>
+    <>
+      <div className="h-screen w-screen flex justify-center items-center text-2xl">
+        <Link href={"/configuracoes-perfil"}>Ir para configurações de perfil</Link>
+      </div>
+    </>
   )
 }
